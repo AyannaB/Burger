b3e15e0c7069f340441786e39b182064f1bd6410
Ayanna Burrowes <a.burrowes529@gmail.com>
2019-09-07 21:34:18 -0400

    updating all files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devour) VALUES ('Hamburger', true);
+INSERT INTO burgers (name, devour) VALUES ('Black Bean Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Cheeseburger', true);
+
